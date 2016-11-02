231fa2823d8ca6d34ee7acb9471c9b585bbbbbfb
Kenaz Kwa <kenazk@kenaz2.local>
2016-11-01 22:34:01 -0700

    Saving

--- js/feed.js
+++ js/feed.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     var url = 'http://fetchrss.com/rss/58191cb88a93f87f4ee0dfdd22358496719.xml';
     var description = document.getElementById('body')
 
-/*
+
     feednami.load(url,function(result){
         if(result.error) {
             console.log(result.error);
@@ -12,11 +12,5 @@ $(document).ready(function() {
             console.log(entries[0].description)
             description.innerHTML = entries[0].description
         }
-    });
-*/
-    feednami.load(url,function(result){
-      var entries = result.feed.entries;
-      console.log(entries[0].description)
-      description.innerHTML = entries[0].description
     });
 });
